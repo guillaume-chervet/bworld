cce5ac51364cbd85c3f4cfd72ebc2f6ec1eb80fd
Guillaume Chervet <guillaume.chervet@gmail.com>
2019-09-19 18:00:40 +0200

    fix(menuAside) menuItem alwys visible

--- src/Demo.Mvc.Core/ClientApp/src/free/user/mwUser-component.js
+++ src/Demo.Mvc.Core/ClientApp/src/free/user/mwUser-component.js
@@ -1,4 +1,5 @@
 ﻿import app from '../../app.module';
+import {dateString} from '../../shared/date/date-factory';
 
 import React from 'react';
 import { react2angular } from 'react2angular';
@@ -10,10 +11,10 @@ export const User = ({ data }) => {
         <p className="mw-author">
           <a rel="me" href={data.userInfo.authorUrl ? data.userInfo.authorUrl : ''}>
             <span itemProp="author">{data.userInfo.userName ? data.userInfo.userName : 'unknow'}</span>
-          </a> le <span>{data.createDate.toString('dd/MM/yyyy à HH:mm')}</span>
-            {data.lastUpdateUserInfo.userName ? (<span>, mis à jour par <a rel="me" href={data.lastUpdateUserInfo.authorUrl ? data.lastUpdateUserInfo.authorUrl : ''}>
+          </a> <span>{dateString(data.createDate.getTime())}</span>
+            {data.lastUpdateUserInfo.userName ? (<span>, mis à jour {data.userInfo.userName !== data.lastUpdateUserInfo.userName ? (<>par <a rel="me" href={data.lastUpdateUserInfo.authorUrl ? data.lastUpdateUserInfo.authorUrl:  ''}>
                     <span itemProp="author">{data.lastUpdateUserInfo.userName ? data.lastUpdateUserInfo.userName :  'unknow'}</span>
-                </a> le {data.updateDate.toString('dd/MM/yyyy à HH:mm')}</span>
+                </a></>) : null} {dateString(data.updateDate.getTime())}</span>
                 ): null}
         </p>
   );
